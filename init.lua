ae9f0c34db296368e1d8212c040ea8067c9f73dc
L1nx32b <3118029164@qq.com>
2026-03-15 16:10:15 +0800

    NVIM config update (3-15)

---
+++ init.lua
@@ -0,0 +1,54 @@
+vim.opt.number = true -- 显示行号
+vim.wo.cursorline = true -- 当前行号高亮显示
+-- Display tabs and trailing spaces
+vim.opt.list = true
+vim.opt.listchars = { tab = ">-", trail = "-" }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+vim.opt.scrolloff = 15
+vim.opt.sidescrolloff = 10
+vim.opt.startofline = false
+
+
+vim.opt.conceallevel = 2
+
+vim.wo.wrap = false
+
+-- Tab related options
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- bootstrap lazy.nvim, LazyVim and your pluginos
+require("config.lazy") -- Import './lua/confvim.opt.number = true
+
+require("utils.keymapping") -- Import './lua/utils/keymapping.lua
+
+-- Snacks profiler
+if vim.env.PROF then
+  -- example for lazy.nvim
+  -- change this to the correct path for your plugin manager
+  local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
+  vim.opt.rtp:append(snacks)
+  require("snacks.profiler").startup({
+    startup = {
+      event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  })
+end
